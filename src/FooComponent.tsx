5165b97e19d21ce79ccd1fc9a528948b04917dff
Lavshyak <lavshyak@gmail.com>
2024-07-26 23:52:49 +0500

    now it works

--- src/FooComponent.tsx
+++ src/FooComponent.tsx
@@ -1,22 +1,16 @@
-import {useInjection} from "inversify-react";
+import {resolve, useInjection} from "inversify-react";
 import {Bar} from "./Bar";
 import {Component} from "react";
 import SERVICE_IDENTIFIER from "./identifiers";
-import container from "./ioc_config";
-import getDecorators from "inversify-inject-decorators";
 
-const {lazyInject} = getDecorators(container);
-console.log(JSON.stringify(lazyInject))
-
-//it doesn't work
+//it works
 export class FooComponent extends Component
 {
-    @lazyInject(SERVICE_IDENTIFIER.BAR)
+    @resolve(SERVICE_IDENTIFIER.BAR)
     private readonly bar!: Bar
 
     render()
     {
-        //this.bar is undefined
         console.log("bar value: " + this.bar.v);
         return (<div>bar value: {this.bar.v}</div>)
     }
